e2471a91c1b2cdcca7ad20dd30e5fa71433be371
Erica Rottman <erica7650@gmail.com>
2016-12-27 15:24:35 -0800

    update question 4

--- main.js
+++ main.js
@@ -10,33 +10,57 @@
 */
 
 
-$(document).ready(function() {  
-   
+$(document).ready(function() {
 
-    
+
+
 // Question #1
+
+$(".changeColor").click(function () {
+  $("li:nth-child(3)").addClass("alt-color")
+
+});
+
 
 
 // Question #2
 
+$("li:nth-child(4)").click(function () {
+
+  $("li:nth-child(4)").append($("li:nth-child(4").clone(false));
+});
+
 
 // Question #3
 
+$("#removeLi").click(function () {
+
+  $("li:nth-child(2)").remove();
+});
+
 
 // Question #4
-    
-    
-// Question #5  
-  
 
-  // Question #6	
-	
+$("li:last").click(function () {
 
-	
+  $("li:last").addClass("altfont-size");
+
+  $("li:nth-child(1n+2)").remove();
+
+});
+
+
+// Question #5
+
+
+  // Question #6
+
+
+
 // Question #7
-  
-  
-  
+
+
+
   // Question #8
 
 
